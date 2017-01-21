997929a0a815f26dfb2399d8619465b45e5fc028
czheo <czheo1987@gmail.com>
2017-01-20 18:30:29 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="syntax_sugar",
+    version="0.0.1",
+    url='https://github.com/czheo/syntax_sugar_python',
+    description="add sweety syntax sugars to Python",
+    author="czheo",
+    license="MIT",
+    keywords="syntax, functional",
+    packages=find_packages(),
+)

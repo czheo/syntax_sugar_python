95601e99859128338f9766d23912db104b7a18a5
czheo <czheo1987@gmail.com>
2017-01-22 02:28:21 -0800

    fix receipes

--- receipes/cat.py
+++ receipes/cat.py
@@ -1,5 +1,6 @@
 from syntax_sugar import *
 from sys import argv
+from functools import partial
 
 
-pipe(argv[1:]) | each(lambda filename: open(filename).read()) | concat | puts
+pipe(argv[1:]) | each(lambda filename: open(filename).read()) | concat | partial(print, end='')

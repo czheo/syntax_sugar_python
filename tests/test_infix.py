4359c90854f259acbe53a2c071442126f2329118
Tercio de Melo <terciodemelo@gmail.com>
2017-01-23 20:27:56 -0300

    Adds unit tests to /is_a/ infix

--- tests/test_infix.py
+++ tests/test_infix.py
@@ -7,3 +7,27 @@ def test_str_to_str():
     assert str('A' /to/ 'Z') == 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 def test_take():
     assert 1 /to/ INF /take/ 5 == [1,2,3,4,5]
+
+def test_is_a():
+    values_types_right = [
+        (2, int),
+        ('strings', str),
+        ({}, dict),
+        ([], list),
+        ((), tuple)
+    ]
+
+    values_types_wrong = [
+        (2, [str, dict, list, tuple]),
+        ('strings', [int, dict, list, tuple]),
+        ({}, [int, str, list, tuple]),
+        ([], [int, str, dict, tuple]),
+        ((), [int, str, dict, list])
+    ]
+
+    for value, type in values_types_right:
+        assert value /is_a/ type
+            
+    for value, types in values_types_wrong:
+        for type in types:
+            assert not value /is_a/ type

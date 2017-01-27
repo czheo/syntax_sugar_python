8c7de246695d36196ad7ca6664f97c7480f02e14
czheo <czheo1987@gmail.com>
2017-01-27 00:19:59 -0800

    recursive compose -> iteration

--- syntax_sugar/composable.py
+++ syntax_sugar/composable.py
@@ -15,9 +15,6 @@ class composable(partial):
         return lambda *args, **kwargs: func(self(*args, **kwargs))
 
 def compose(*args):
-    if len(args) == 1:
-        return args[0]
-    if len(args) == 2:
-        return lambda *ag, **kwag: args[0](args[1](*ag, **kwag))
-    else:
-        return compose(compose(args[:-1]), args[-1])
+    return reduce(lambda acc, fn:
+        (lambda *ag, **kwag: acc(fn(*ag, **kwag))),
+    args)

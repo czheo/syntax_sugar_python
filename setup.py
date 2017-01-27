6bed842c137386ff79582479ab254a7228bff050
czheo <czheo1987@gmail.com>
2017-01-26 23:44:05 -0800

    bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="syntax_sugar",
-    version="0.0.8",
+    version="0.1.0",
     url='https://github.com/czheo/syntax_sugar_python',
     description="add syntactic sugar to Python",
     author="czheo",
